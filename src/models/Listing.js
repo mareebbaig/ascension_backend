33b1ba91cd80fef3463c6c89cd9ecac80994b0df
Taha Aslam <tahaaslam045@gmail.com>
2023-03-02 06:15:27 +0500

    updated

--- src/models/Listing.js
+++ src/models/Listing.js
@@ -5,6 +5,6 @@ module.exports = function Listing() {
         createListing:
             "insert into listing(id,title,description,reason_for_selling,industry,location,is_auctioned,is_established,price,seller,assets,opportunities,risks) values ($(id),$(title),$(description),$(reason_for_selling),$(industry),$(location),$(is_auctioned),$(is_established),$(price),$(seller), $(assets), $(opportunities),$(risks)) returning id",
         getSingleListing:
-            "select Li.title, Li.description ,  li.reason_for_selling , Li.is_auctioned , Li.is_established , Li.seller , Ind.label as industry , C.label AS country , P.asking_price , P.cash_flow , P.gross_revenue , P.inventory_price , P.net_income  from listing AS Li INNER JOIN industries AS Ind on Li.industry = Ind.id INNER JOIN locations AS Lo ON Li.location = Lo.id INNER JOIN countries AS C on C.id = Lo.country INNER JOIN prices AS P ON Li.price = p.id where Li.id = ${businessId}",
+            "select Li.title, Li.description ,  li.reason_for_selling , Li.is_auctioned , Li.is_established , Li.seller , Ind.label as industry , C.label AS country , P.asking_price , P.cash_flow , P.gross_revenue , P.inventory_price , P.net_income , Li.assets, Li.opportunities, Li.risks  from listing AS Li INNER JOIN industries AS Ind on Li.industry = Ind.id INNER JOIN locations AS Lo ON Li.location = Lo.id INNER JOIN countries AS C on C.id = Lo.country INNER JOIN prices AS P ON Li.price = p.id where Li.id = ${businessId}",
     };
 };
